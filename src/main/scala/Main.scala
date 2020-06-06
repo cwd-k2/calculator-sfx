ed4bef64f0f090747d50e4acfd258ff6bd19e8ec
cwd-k2 <kaito.augmented7th@gmail.com>
2020-06-06 12:32:59 +0900

    GridPane を用いて各種ボタンを追加

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,86 +1,54 @@
-/*
-import scalafx.application.JFXApp
-import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
-import scalafx.scene.Scene
-import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color._
-import scalafx.scene.paint.{Stops, LinearGradient}
-import scalafx.scene.text.Text
-
-object Main extends JFXApp {
-
-  stage = new PrimaryStage {
-    title = "ScalaFX Hello World"
-    scene = new Scene {
-      fill = Black
-      content = new HBox {
-        padding = Insets(20)
-        children = Seq(
-          new Text {
-            text = "Hello "
-            style = "-fx-font-size: 48pt"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(PaleGreen, SeaGreen))
-          },
-          new Text {
-            text = "World!!!"
-            style = "-fx-font-size: 48pt"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Cyan, DodgerBlue)
-            )
-            effect = new DropShadow {
-              color = DodgerBlue
-              radius = 25
-              spread = 0.25
-            }
-          }
-        )
-      }
-    }
-  }
-}
-*/
-
 import scalafx.Includes._
+
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+
 import scalafx.scene.Scene
 import scalafx.scene.control.Button
 import scalafx.scene.layout.VBox
-
 import scalafx.scene.layout.GridPane
 
-//val all_buttons = ("1", "2", "3", "4",
-//                  "5", "6", "7", "8",
-//                  "9", "0", "/", "*",
-//                  "-", "+", "=", "C")
+import scalafx.event.ActionEvent
+import scalafx.event.EventHandler
 
 object Main extends JFXApp {
+  def makeButton(a: String): Button =
+    new Button {
+      text = a
+      onAction = (e: ActionEvent) => println(a)
+    }
+
+  val buttonGrid = new GridPane
+  buttonGrid.add(makeButton("("),   0, 0, 1, 1)
+  buttonGrid.add(makeButton(")"),   1, 0, 1, 1)
+  buttonGrid.add(makeButton("DEL"), 2, 0, 1, 1)
+  buttonGrid.add(makeButton("AC"),  3, 0, 1, 1)
+
+  buttonGrid.add(makeButton("7"), 0, 1, 1, 1)
+  buttonGrid.add(makeButton("8"), 1, 1, 1, 1)
+  buttonGrid.add(makeButton("9"), 2, 1, 1, 1)
+  buttonGrid.add(makeButton("/"), 3, 1, 1, 1)
+
+  buttonGrid.add(makeButton("4"), 0, 2, 1, 1)
+  buttonGrid.add(makeButton("5"), 1, 2, 1, 1)
+  buttonGrid.add(makeButton("6"), 2, 2, 1, 1)
+  buttonGrid.add(makeButton("*"), 3, 2, 1, 1)
+
+  buttonGrid.add(makeButton("1"), 0, 3, 1, 1)
+  buttonGrid.add(makeButton("2"), 1, 3, 1, 1)
+  buttonGrid.add(makeButton("3"), 2, 3, 1, 1)
+  buttonGrid.add(makeButton("-"), 3, 3, 1, 1)
+
+  buttonGrid.add(makeButton("0"), 0, 4, 1, 1)
+  buttonGrid.add(makeButton("."), 1, 4, 1, 1)
+  buttonGrid.add(makeButton("="), 2, 4, 1, 1)
+  buttonGrid.add(makeButton("+"), 3, 4, 1, 1)
+
   stage = new PrimaryStage {
-    title = "Hello"
+    title = "Calculator with ScalaFX"
     scene = new Scene {
       root = new VBox {
-        /*
-        children = new Button("Hello Button") {
-          onMouseClicked = handle {
-            println("hello")
-          }
-        }
-        */
-        children = Seq(
-          GridPane = {
-            for(button <- ("1", "2", "3", "4",
-            "5", "6", "7", "8",
-            "9", "0", "/", "*",
-            "-", "+", "=", "C")) yield new Button(button)
-          }
-          //new Button("("),
-          //new Button(")"),
-        )
+        children = buttonGrid
       }
     }
   }
