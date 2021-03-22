583a89cce3e1e4da5b922d08b223b2b6fb7b128b
cwd-k2 <kaito.augmented7th@gmail.com>
2021-03-22 23:39:13 +0900

    利用されていないファイルを削除

--- src/main/scala/CalculatorButton.scala
+++
@@ -1,6 +0,0 @@
-// import scalafx.event.ActionEvent
-// import scalafx.scene.control.Button
-//
-// class CalculatorButton(val calculator: Calculator) extends Button {
-// //    onAction = (e: ActionEvent) => calculator.input(text)
-// }
--- src/main/scala/CalculatorDisplay.scala
+++
@@ -1,9 +0,0 @@
-// import scalafx.event.ActionEvent
-// import scalafx.scene.control.TextField
-//
-// class CalculatorDisplay(val calc: Calculator) extends TextField {
-//   text = ""
-//   calc.onChange = (e: ActionEvent) => {
-//     text = e.text
-//   }
-// }
